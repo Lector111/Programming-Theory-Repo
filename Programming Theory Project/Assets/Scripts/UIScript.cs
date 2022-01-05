9eeef7ff7e3200b3587ea12d5095e331bcd5a5e0
Lector111 <lectorcorp@inbox.lv>
2022-01-05 20:08:39 +0200

    title screen creation

---
+++ Programming Theory Project/Assets/Scripts/UIScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+    public void MenuStartApp()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void MenuExit()
+    {
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit(); // original code to quit Unity player
+        #endif
+    }
+
+    public void MainExitMenu()
+    {
+        SceneManager.LoadScene("title-screen");
+    }
+}
